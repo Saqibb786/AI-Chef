7af4771467078e1beaeff5e5aea5f71263f39bf0
Saqib Ali Butt <saqibbutt10000@gmail.com>
2026-05-03 22:25:50 +0500

    refactor: seprate components

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function IngredientsList(props) {
+        const ingredientsListItems = props.ingredientsList.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    
+  return (
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list"
+                aria-live="polite">{ingredientsListItems}</ul>
+                {props.ingredientsList.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.toggleRecipeShown}>
+                        Get a recipe
+                    </button>
+                </div>}
+            </section>
+  )
+}
+
+export default IngredientsList
