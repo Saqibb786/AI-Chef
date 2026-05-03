915cd3fb058d8457fe1929ea46e9f1079aab20bf
Saqib Ali Butt <saqibbutt10000@gmail.com>
2026-05-03 16:46:35 +0500

    refactor: add conditional rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,6 +13,7 @@ function Main() {
         
     }
 
+
   return (
         <main>
             <form className="add-ingredient-form" action={addIngredient}>
@@ -25,7 +26,18 @@ function Main() {
                 <button>Add ingredient</button>
             </form>
 
-            {ingredientsListItems}
+            {ingredientsList.length > 0 ? 
+                <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section> : null}
         </main>
   )
 }
